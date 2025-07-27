fbcde8e8c5156ef27d016dfef8525a97a54cea21
Amirhossein Bayat <36699831+amirhb29@users.noreply.github.com>
2025-07-27 19:28:49 +0100

    fix: improve crc32 implementation

--- tofcore/include/tofcore/crc32.h
+++ tofcore/include/tofcore/crc32.h
@@ -8,7 +8,20 @@
  */
 #   include <stdint.h>
 
-uint32_t calcCrc32(const uint8_t *buffer, unsigned int length);
-uint32_t updateCrc32(uint32_t crc, const uint8_t *buffer, unsigned int length);
+#include <cstddef>
+
+/// Calculate the CRC32 of a buffer
+///
+/// @param buffer Pointer to the data
+/// @param length Number of bytes in the buffer
+uint32_t calcCrc32(const uint8_t *buffer, std::size_t length);
+
+/// Update a running CRC32 calculation
+///
+/// @param crc    Existing CRC value
+/// @param buffer Pointer to next chunk of data
+/// @param length Number of bytes in the chunk
+/// @return New CRC value
+uint32_t updateCrc32(uint32_t crc, const uint8_t *buffer, std::size_t length);
 
 #endif // CRC32_H
